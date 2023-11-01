f01d56634a8607fb25b284203011b32d3ccc917f
DESKTOP-NBOFPMA\lmchi <chien.leminh@hcmut.edu.vn>
2023-11-01 23:41:36 +0700

    Ex7

---
+++ bai7/Core/Inc/set_init.h
@@ -0,0 +1,15 @@
+/*
+ * set_init.h
+ *
+ *  Created on: Oct 10, 2023
+ *      Author: lmchi
+ */
+
+#ifndef INC_SET_INIT_H_
+#define INC_SET_INIT_H_
+
+#include "main.h"
+
+void set_init();
+
+#endif /* INC_SET_INIT_H_ */
